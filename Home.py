0e4adc6a63b6a62f62ffb6e166223a6e22cfe06c
Leonardo Andrade <leo192@gmail.com>
2025-03-26 13:53:56 -0300

    Criadas variáveis de ambiente para customização da tela

--- Home.py
+++ Home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+HOME_TEXT = st.secrets["HOME_TEXT"]
+IMG_URL = st.secrets["IMG_URL"]
 
 # Configuração da página para ocupar toda a largura
 st.set_page_config(
@@ -42,26 +44,12 @@ st.markdown(
 
 # Centralizando conteúdo na largura total da tela
 with st.container():
-    st.image("https://samserv.com.br/img/logo.png", width=250)
+    st.image(IMG_URL, width=250)
 
     st.title("📢 Bem-vindo ao Canal de Relatos Psicossociais")
 
     st.markdown(
-        """
-        <div class="main-content">
-            <p style="font-size:18px; line-height:1.6; color:white; text-align:justify;">
-            A <b>Samserv</b> valoriza o bem-estar e a saúde mental de todos os colaboradores. Para reforçar esse compromisso, criamos este espaço dedicado ao relato de situações que possam impactar o ambiente de trabalho sob a perspectiva psicossocial.
-            <br><br>
-            Aqui, você pode compartilhar de forma segura e confidencial experiências relacionadas a <b>pressões excessivas, assédio moral, terrorismo psicológico, discriminação, dificuldades na conciliação entre vida profissional e pessoal</b>, além de outros fatores que possam comprometer sua saúde emocional e o clima organizacional.
-            <br><br>
-            <b>Seu feedback é essencial</b> para que possamos identificar e tratar essas questões, promovendo um ambiente de trabalho mais saudável, respeitoso e produtivo para todos.
-            <br><br>
-            Fique à vontade para relatar sua experiência de maneira objetiva e sincera. Suas informações serão tratadas com <b>seriedade e discrição</b>.
-            <br><br>
-            <b>Juntos, podemos construir um ambiente de trabalho mais equilibrado e acolhedor.</b>
-            </p>
-        </div>
-        """,
+        HOME_TEXT,
         unsafe_allow_html=True
     )
 
